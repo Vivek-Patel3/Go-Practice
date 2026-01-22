e06ad023df7e1ae4566f065042629f5fef3cf486
Vivek-Patel3 <vivekvibha310@gmail.com>
2026-01-22 19:06:38 +0530

    Covered Struct

--- main/app.go
+++ main/app.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"errors"
+)
+
 func main() {
-	modifyArray()	
+	title, content, err := getNoteDetails()
+
+	if(err != nil) {
+		fmt.Println(err.Error());
+	}
+}
+
+func getNoteDetails() (string, string, error) {
+	title, err := getInputFromUser("Title:")		
+
+	if(err != nil) {
+		fmt.Println(err.Error())
+		return "","",err
+	}
+
+	content, err := getInputFromUser("Content:")
+
+	if(err != nil) {
+		fmt.Println(err.Error())
+		return  title,"",err
+	}
+
+	return title,content,nil
+}
+
+func getInputFromUser(prompt string) (string, error) {
+	fmt.Print(prompt)
+	var value string
+	fmt.Scan(&value)
+
+	if(value == "") {
+		return "",errors.New("Cannot be empty value")
+	}
+
+	return value,nil
 }
---
+++ main/r1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func random() {
+	var a string
+	fmt.Scan(&a)
+
+	fmt.Println(a)
+}
