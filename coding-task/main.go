ac9993a488e21463f8efb1b6f69bf5cd010e80a4
Vivek-Patel3 <vivekvibha310@gmail.com>
2026-02-12 12:51:57 +0530

    implement graceful shutdown: - stop the producer from producing more events - close the channel sending events to consumers - wait for consumers to finish sending events to the process channel - close the process channel - let the producers finish processing the remaining events

--- coding-task/main.go
+++ coding-task/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"math/rand"
+	"os"
 	"os/signal"
 	"strconv"
 	"strings"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/Vivek-Patel3/coding-task/internal/event"
@@ -19,7 +22,7 @@ func process(event event.Event) {
 	// converting the payload to uppper case
 	event.Payload = strings.ToUpper(event.Payload)
 
-	fmt.Println("Processed payload:", event.Payload)
+	fmt.Printf("Event: %v processed\n",event.Id)
 	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
 
 	// write to the in-memory map
@@ -38,28 +41,36 @@ func main() {
 	processChan := make(chan event.Event, 10)
 	storageDisk = make(map[int]string)
 
+	ctx, cancel := context.WithCancel(context.Background())
+	
 	var wg sync.WaitGroup
+	var consumer_wg sync.WaitGroup
 
 	// producer - produces infinite events
 	// one producer -> events for lifetime
-	go func() {
+	go func(ctx context.Context) {
 		for i:=0; ; i++ {
-			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-			eventsChan <- event.Event {
-				Id: i,
-				Payload: "hello from " + strconv.Itoa(i),
+			select {
+				case <-ctx.Done():
+					return
+				case eventsChan <- event.Event {
+						Id: i,
+						Payload: "hello from " + strconv.Itoa(i),
+					}:
 			}
 		}
-	}()
+	}(ctx)
 
 	// multiple consumers as goroutines consume these events
 	// fan out to consumers
 	for i:=0;i<20;i++ {
+		consumer_wg.Add(1)
 		go func() {
+			defer consumer_wg.Done()
 			for event := range eventsChan {
 				// send to processor-channel
 				processChan <- event
-				fmt.Println("event consumed with payload:", event.Payload)
+				fmt.Printf("Event: %v consumed\n", event.Id)
 			}
 		}()
 	}
@@ -82,6 +93,28 @@ func main() {
 	3. Drain the channels -> consumers consume the remaining events
 						  -> processors finish processing the remaining events
 	*/
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT)
+
+	if sig, ok := <-signalChan; ok {
+		// stop the producer
+		cancel()
+
+		// close the channels
+		close(eventsChan)
+		
+		// need to wait till all the consumers drain the eventsChan and push those events to processChan
+		// therefore need to wait for all consumer's goroutines to finish, so add waitgroup for consumers 
+		consumer_wg.Wait()
+
+		// now close the processChan
+		close(processChan)
+		fmt.Println("Terminating due to", sig)
+	}
 
 	wg.Wait()
+
+	for k,v := range storageDisk {
+		fmt.Printf("Event %v: %s\n", k, v)
+	}
 }
